9f5bc4886b392fd52b8e55323571af5e78010272
Matthew Leach <dev@mattleach.net>
2024-04-06 15:50:49 +0100

    poll: add tests
    Add tests to the poll module.

--- src/reactor/poll.rs
+++ src/reactor/poll.rs
@@ -104,3 +104,112 @@ impl<T: Send + Sync> Poll<T> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{os::fd::OwnedFd, sync::Arc, thread::sleep, time::Duration};
+
+    use nix::unistd::{pipe, write};
+
+    use crate::reactor::WakeupKind;
+
+    use super::Poll;
+
+    fn setup_test() -> (Arc<OwnedFd>, Arc<OwnedFd>, Poll<u32>) {
+        let (fd_rd, fd_wr) = pipe().unwrap();
+        let fd_rd = Arc::new(fd_rd);
+        let fd_wr = Arc::new(fd_wr);
+        let poll: Poll<u32> = Poll::new();
+
+        (fd_rd, fd_wr, poll)
+    }
+
+    #[test]
+    fn single_wakeup_read() {
+        let (fd_rd, fd_wr, poll) = setup_test();
+
+        poll.insert(fd_rd.clone(), 1, WakeupKind::Readable);
+
+        let t1 = std::thread::spawn(move || {
+            assert_eq!(poll.wait(), 1);
+
+            assert!(poll.subscriptions.lock().unwrap().len() == 0);
+        });
+
+        write(fd_wr, &[2]).unwrap();
+
+        t1.join().unwrap();
+    }
+
+    #[test]
+    fn single_wakeup_write() {
+        let (_fd_rd, fd_wr, poll) = setup_test();
+
+        poll.insert(fd_wr.clone(), 1, WakeupKind::Writable);
+
+        let t1 = std::thread::spawn(move || {
+            assert_eq!(poll.wait(), 1);
+            write(fd_wr, &[2]).unwrap();
+        });
+
+        t1.join().unwrap();
+    }
+
+    #[test]
+    fn multi_wakeup_same_fd_read() {
+        let (fd_rd, fd_wr, poll) = setup_test();
+
+        poll.insert(fd_rd.clone(), 1, WakeupKind::Readable);
+        poll.insert(fd_rd.clone(), 2, WakeupKind::Readable);
+        poll.insert(fd_rd.clone(), 3, WakeupKind::Readable);
+
+        let t1 = std::thread::spawn(move || {
+            assert_eq!(poll.wait(), 1);
+            assert!(poll.subscriptions.lock().unwrap().len() == 1);
+            assert_eq!(poll.wait(), 2);
+            assert!(poll.subscriptions.lock().unwrap().len() == 1);
+            assert_eq!(poll.wait(), 3);
+            assert!(poll.subscriptions.lock().unwrap().len() == 0);
+        });
+
+        write(fd_wr, &[2]).unwrap();
+
+        t1.join().unwrap();
+    }
+
+    #[test]
+    fn multi_wakeup_dual_fds_read() {
+        let (fd_rd, fd_wr, poll) = setup_test();
+        let (fd2_rd, fd2_wr, _) = setup_test();
+
+        poll.insert(fd_rd.clone(), 1, WakeupKind::Readable);
+        poll.insert(fd_rd.clone(), 2, WakeupKind::Readable);
+        poll.insert(fd_rd.clone(), 3, WakeupKind::Readable);
+        poll.insert(fd2_rd.clone(), 4, WakeupKind::Readable);
+        poll.insert(fd2_rd.clone(), 5, WakeupKind::Readable);
+        poll.insert(fd2_rd.clone(), 6, WakeupKind::Readable);
+
+        let t1 = std::thread::spawn(move || {
+            assert_eq!(poll.wait(), 4);
+            assert!(poll.subscriptions.lock().unwrap().len() == 2);
+            assert_eq!(poll.wait(), 5);
+            assert!(poll.subscriptions.lock().unwrap().len() == 2);
+            assert_eq!(poll.wait(), 6);
+            assert!(poll.subscriptions.lock().unwrap().len() == 1);
+            assert_eq!(poll.wait(), 1);
+            assert!(poll.subscriptions.lock().unwrap().len() == 1);
+            assert_eq!(poll.wait(), 2);
+            assert!(poll.subscriptions.lock().unwrap().len() == 1);
+            assert_eq!(poll.wait(), 3);
+            assert!(poll.subscriptions.lock().unwrap().len() == 0);
+        });
+
+        write(fd2_wr, &[2]).unwrap();
+
+        sleep(Duration::from_secs(1));
+
+        write(fd_wr, &[2]).unwrap();
+
+        t1.join().unwrap();
+    }
+}
