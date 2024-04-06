1d0800b3553338dc12221fb4bae8b971c8efa311
Matthew Leach <dev@mattleach.net>
2024-04-06 18:46:31 +0100

    udp: document
    Add docs for the UDP futures module.

--- src/futures/udp.rs
+++ src/futures/udp.rs
@@ -1,3 +1,27 @@
+//! Async UDP Sockets.
+//!
+//! This module implements an async version of [std::net::UdpSocket].
+//! It allows reception and transmission to and from UDP sockets to
+//! be defered by `.await`ing the return values of
+//! [UdpSocket::recv_from] and [UdpSocket::send_to].
+//!
+//! # Example
+//!
+//! Here is an example of an async UDP echo server.
+//! ```no_run
+//! use std::net::Ipv4Addr;
+//! use trale::futures::udp::UdpSocket;
+//! async {
+//!     let mut sock = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 8888))?;
+//!     let mut buf = [0u8; 1500];
+//!     loop {
+//!         let (len, src) = sock.recv_from(&mut buf).await?;
+//!         sock.send_to(&buf[..len], src).await?;
+//!     }
+//!#     Ok::<(), std::io::Error>(())
+//! };
+//! ```
+
 use std::future::Future;
 use std::io;
 use std::io::Result;
@@ -12,11 +36,15 @@ use std::task::Poll;
 use crate::reactor::Reactor;
 use crate::reactor::WakeupKind;
 
+/// An async datagram socket.
 pub struct UdpSocket {
     inner: Arc<StdUdpSocket>,
 }
 
 impl UdpSocket {
+    /// Attempt to bind to the local address `A` and return a new
+    /// [UdpSocket].  [Self::recv_from] and [Self::send_to] can then
+    /// be called on this socket once bound.
     pub fn bind<A: ToSocketAddrs>(addr: A) -> Result<Self> {
         let sock = StdUdpSocket::bind(addr)?;
 
@@ -27,6 +55,20 @@ impl UdpSocket {
         })
     }
 
+    /// Wait for reception of a datagram.
+    ///
+    /// # Example
+    ///
+    /// ```no_run
+    /// use std::net::Ipv4Addr;
+    /// use trale::futures::udp::UdpSocket;
+    /// async {
+    ///     let mut sock = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 0))?;
+    ///     let mut buf = [0u8; 1500];
+    ///     let (len, src) = sock.recv_from(&mut buf).await?;
+    ///#     Ok::<(), std::io::Error>(())
+    /// };
+    /// ```
     pub fn recv_from<'a>(&'a mut self, buf: &'a mut [u8]) -> RecvFrom {
         RecvFrom {
             sock: self.inner.clone(),
@@ -34,6 +76,20 @@ impl UdpSocket {
         }
     }
 
+    /// Wait for reception of a datagram.
+    ///
+    /// # Example
+    ///
+    /// ```no_run
+    /// use std::net::Ipv4Addr;
+    /// use trale::futures::udp::UdpSocket;
+    /// async {
+    ///     let sock = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, 0))?;
+    ///     let buf = [0xad, 0xbe, 0xef];
+    ///     sock.send_to(&buf, (Ipv4Addr::new(192, 168, 0, 1), 8080)).await?;
+    ///#     Ok::<(), std::io::Error>(())
+    /// };
+    /// ```
     pub fn send_to<'a, A: ToSocketAddrs>(&'a self, buf: &'a [u8], target: A) -> SendTo<A> {
         SendTo {
             sock: self.inner.clone(),
@@ -43,6 +99,8 @@ impl UdpSocket {
     }
 }
 
+/// A future that receives data into a datagram socket, see
+/// [UdpSocket::recv_from].
 pub struct RecvFrom<'a> {
     sock: Arc<StdUdpSocket>,
     buf: &'a mut [u8],
@@ -68,6 +126,8 @@ impl Future for RecvFrom<'_> {
     }
 }
 
+/// A future that send data from a datagram socket, see
+/// [UdpSocket::send_to].
 pub struct SendTo<'a, A: ToSocketAddrs> {
     sock: Arc<StdUdpSocket>,
     dst: A,
